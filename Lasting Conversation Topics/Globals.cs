2258f3f509cbb1796c45457c88556477cc74e3cc
sophiesalacia <sophiesalacia@users.noreply.github.com>
2022-06-21 05:11:25 -0400

    Lasting Conversation Topics initial commit.

---
+++ Lasting Conversation Topics/Globals.cs
@@ -0,0 +1,30 @@
+﻿using StardewModdingAPI;
+using StardewModdingAPI.Events;
+
+namespace LastingCTs
+{
+    internal class Globals
+    {
+        public static IManifest Manifest { get; set; }
+        public static IModHelper Helper { get; set; }
+        public static ICommandHelper CCHelper => Helper.ConsoleCommands;
+        public static IGameContentHelper GameContent => Helper.GameContent;
+        public static IModContentHelper ModContent => Helper.ModContent;
+        public static IContentPackHelper ContentPackHelper => Helper.ContentPacks;
+        public static IDataHelper DataHelper => Helper.Data;
+        public static IInputHelper InputHelper => Helper.Input;
+        public static IModEvents EventHelper => Helper.Events;
+        public static IMultiplayerHelper MultiplayerHelper => Helper.Multiplayer;
+        public static IReflectionHelper ReflectionHelper => Helper.Reflection;
+        public static ITranslationHelper TranslationHelper => Helper.Translation;
+        public static string UUID => Manifest.UniqueID;
+        public static string ContentPath => "sophie.ConversationTopics";
+
+        internal static void InitializeGlobals(ModEntry modEntry)
+        {
+            Manifest = modEntry.ModManifest;
+            Helper = modEntry.Helper;
+            Log.Monitor = modEntry.Monitor;
+        }
+    }
+}
---
+++ Lasting Conversation Topics/README.md
@@ -0,0 +1,15 @@
+﻿
+#### Lasting Conversation Topics
+*by sophie*
+
+Extends the provided conversation topics indefinitely. Handles 'Introduction' by default; other CTs can be added by targeting the asset `sophie.ConversationTopics` and adding entries in the format `"ConversationTopicName": "true"`. See example pack on Nexus for details.
+
+[NexusMods]()  
+[GitHub]()  
+TO DO
+
+#### If you have any issues:
+* Read the documentation carefully. I have tried to be as thorough as possible.
+* Make sure SMAPI and Content Patcher are up-to-date.
+* You can reach me on the Stardew Valley discord (sophiesalacia#0001) or on the Nexus mod page.
+* Please provide a SMAPI log, as well as your mod files, so that I can assist you better.
