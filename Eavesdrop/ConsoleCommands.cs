51f0ae3ae87c90746c449a56d1b6eb61757f5287
sophiesalacia <sophiesalacia@users.noreply.github.com>
2024-09-22 21:34:34 -0400

    Initial commit for Eavesdrop

---
+++ Eavesdrop/ConsoleCommands.cs
@@ -0,0 +1,10 @@
+namespace Eavesdrop;
+
+internal class ConsoleCommands
+{
+    //[ConsoleCommand("test1", "Testing first console command")]
+    //public static void TestConsoleCommand1(string command, string[] args)
+    //{
+    //    Log.Info("Successfully ran console command test1.");
+    //}
+}
---
+++ Eavesdrop/ModEntry.cs
@@ -0,0 +1,10 @@
+namespace Eavesdrop;
+
+public class ModEntry : Mod
+{
+    public override void Entry(IModHelper helper)
+    {
+        Globals.InitializeGlobals(this);
+        Shared.Initialize();
+    }
+}
